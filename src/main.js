51fab95771b6a77a68f1b064ec39b8afd285c216
Olia <olyasemenchuk13@gmail.com>
2026-05-13 22:06:47 +0300

    set the project

---
+++ src/main.js
@@ -0,0 +1,3 @@
+// У файлі main.js напиши всю логіку роботи додатка. Виклики нотифікацій iziToast, усі перевірки на довжину масиву в отриманій відповіді робимо саме в цьому файлі. Імпортуй в нього функції із файлів pixabay-api.js та render-functions.js та викликай їх у відповідний момент.
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
