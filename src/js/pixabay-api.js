6f9bc44fb0985c6c5b38ddb55b38e202d2eff7e2
Olia <olyasemenchuk13@gmail.com>
2026-05-14 23:18:54 +0300

    corrected axios

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,18 +1,18 @@
+import axios from 'axios';
+
 const API_KEY = '55861557-2268365b5b48955757ff78c80';
+const BASE_URL = 'https://pixabay.com/api/';
 
 export default function getImagesByQuery(word) {
-  const params = new URLSearchParams({
-    key: API_KEY,
-    q: word,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-  });
-
-  return fetch(`https://pixabay.com/api/?${params}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
-    return response.json();
-  });
+  return axios
+    .get(BASE_URL, {
+      params: {
+        key: API_KEY,
+        q: word,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+      },
+    })
+    .then(response => response.data);
 }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
-// У файлі main.js напиши всю логіку роботи додатка. Виклики нотифікацій iziToast, усі перевірки на довжину масиву в отриманій відповіді робимо саме в цьому файлі. Імпортуй в нього функції із файлів pixabay-api.js та render-functions.js та викликай їх у відповідний момент.
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-import axios from 'axios';
 import getImagesByQuery from './js/pixabay-api.js';
 import {
   createGallery,
@@ -18,32 +16,37 @@ formEl.addEventListener('submit', formHandler);
 function formHandler(event) {
   event.preventDefault();
 
-  showLoader();
-
   const searchText = input.value.trim();
+
+  if (!searchText) {
+    iziToast.warning({
+      message: 'Please, enter a search query',
+    });
+
+    return;
+  }
 
   input.value = '';
 
+  showLoader();
+  clearGallery();
+
   getImagesByQuery(searchText)
     .then(data => {
-      clearGallery();
-
       if (data.hits.length === 0) {
-        iziToast.show({
-          title: 'Message',
+        iziToast.warning({
           message:
             'Sorry, there are no images matching your search query. Please try again!',
         });
-
         return;
       }
 
       createGallery(data.hits);
     })
     .catch(error => {
-      if (axios.isCancel(error)) {
-        console.log(error);
-      }
+      iziToast.error({
+        message: 'Something went wrong. Please try again later.',
+      });
     })
     .finally(() => {
       hideLoader();
